d0021250a9f8ccb410271adee9da0b6a68a0504b
Roger Watkins <roger.watkins@gnomesoft.co.uk>
2017-08-11 22:54:57 +0100

    Significantly improve error handling on the dashboard.  - Error panel now shown when server error occurs  - Errors do not now prevent the page from refreshing  - Server timeouts handled more gracefully
     Configurable timeouts.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config.use_bearer_token= process.env.CONCOURSE_USE_BEARER || true;
 config.enable_basic_auth = process.env.CONCOURSE_ENABLE_BASIC_AUTH || true;
 config.basic_auth_user = process.env.CONCOURSE_BASIC_AUTH_USER || "admin";
 config.basic_auth_password = process.env.CONCOURSE_BASIC_AUTH_PASSWORD || "password";
-config.http_request_timeout = process.env.HTTP_REQUEST_TIMEOUT || 10000;
+config.http_request_timeout_seconds = process.env.HTTP_REQUEST_TIMEOUT_SECONDS || 5000;
 
 config.concourse_team= process.env.CONCOURSE_TEAM || "main";
 config.concourse_username = process.env.CONCOURSE_USERNAME || "user";
@@ -14,8 +14,10 @@ config.maxAllowedPipelines = 50;
 
 config.enableVersions = process.env.ENABLE_VERSIONS || false;
 config.versions_bucket = process.env.S3_VERSIONS_BUCKET || "s3-versions-bucket";
-config.refresh_in_seconds = process.env.REFRESH_SECONDS || 10;
+config.refresh_pipeline_in_seconds = process.env.REFRESH_PIPELINE_SECONDS || 10;
 config.notify_new_versions_for_minutes = process.env.NOTIFY_NEW_VERSIONS_FOR_MINS || 15;
-config.log_level = process.env.LOG_LEVEL || "trace";
+config.log_level = process.env.LOG_LEVEL || "info";
+
+config.server_timeout_seconds = process.env.SERVER_TIMEOUT_SECONDS || 10000;
 
 module.exports = config;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express'),
     request = require('request'),
     _ = require('lodash'),
     async = require('async'),
+    timeout = require('connect-timeout'),
     basicAuth = require('express-basic-auth'),
     doVersions = require('./lib/versions'),
     doPipelines = require('./lib/pipelines'),
@@ -17,10 +18,14 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/resources'));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(timeout(config.server_timeout_seconds + 's'));
 app.use(bodyParser.json());
+app.use(haltOnTimedout);
+
+let dashboard_user;
 if (config.enable_basic_auth) {
     dashboard_user = {};
-    dashboard_user[config.basic_auth_user] = config.basic_auth_password
+    dashboard_user[config.basic_auth_user] = config.basic_auth_password;
     app.use(basicAuth({
         users: dashboard_user,
         challenge: true,
@@ -38,7 +43,7 @@ const get_bearer = (callback) => {
         },
         json: true,
         strictSSL: false,
-        timeout: config.http_request_timeout
+        timeout: config.http_request_timeout_seconds
     }, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             let token = body.value;
@@ -60,13 +65,28 @@ const ensureAuth = (callback) => {
 
 const doRenderResults = (res) => {
     return function (err, pipelines) {
+        let errors;
         if (err) {
-            res.end(JSON.stringify(err));
+            errors = {message: JSON.stringify(err)};
+            if (!pipelines) {
+                pipelines = [];
+            }
         } else {
-            res.render('overview', {config: config, pipelines: pipelines})
+            errors = {display: 'hide'};
         }
+        res.render('overview', {errors: errors, config: config, pipelines: pipelines});
     };
 };
+
+function haltOnTimedout(req, res, next) {
+    let renderResults = doRenderResults(res);
+    req.on('timeout', function () {
+        logger.error("timeout after " + config.server_timeout_seconds + " seconds");
+        let errors = 'Sadly the request timed out after ' + config.server_timeout_seconds + ' seconds...';
+        renderResults(errors, doPipelines.getPipelineCache());
+    });
+    next();
+}
 
 let lastUpdate;
 let startTime;
@@ -75,10 +95,15 @@ app.get('/', (req, res) => {
     startTime = new Date().getTime();
     let renderResults = doRenderResults(res);
 
-    let refreshInMilliseconds = config.refresh_in_seconds * 1000;
+    let refreshInMilliseconds = config.refresh_pipeline_in_seconds * 1000;
     if (lastUpdate && new Date().getTime() - lastUpdate.getTime() < refreshInMilliseconds) {
         logger.info("Skipping data refresh...");
+        if (req.timedout) {
+            logger.warn("Request already timed out - skip rendering");
+            return;
+        }
         return renderResults(null, doPipelines.getPipelineCache());
+
     }
 
     async.waterfall([
@@ -91,7 +116,13 @@ app.get('/', (req, res) => {
                 logger.info((new Date().getTime() - startTime) / 1000 + " seconds");
                 lastUpdate = new Date();
             }
+            // don't bother sending response if it has already timed out
+            if (req.timedout) {
+                logger.warn("Request already timed out - skip rendering");
+                return;
+            }
             renderResults(err, pipelines);
+
         }
     );
 });
